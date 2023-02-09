b22d092c82a35348bb6a87e45811a0b02d2422a3
Berny Peralta <bernardoperalta260@gmail.com>
2023-02-09 13:46:02 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// Selecting elements
+const updateGrid = document.querySelector(".updateGrid");
+const textCaesar = document.querySelector(".textCaesar");
+const textSquare = document.querySelector(".textSquare");
+
+const abc = {
+  1: "a",
+  2: "b",
+  3: "c",
+  4: "d",
+  5: "e",
+  6: "f",
+  7: "g",
+  8: "h",
+  9: "i",
+  10: "j",
+  11: "k",
+  12: "l",
+  13: "m",
+  14: "n",
+  15: "o",
+  16: "p",
+  17: "q",
+  18: "r",
+  19: "s",
+  20: "t",
+  21: "u",
+  22: "v",
+  23: "w",
+  24: "x",
+  25: "y",
+};
+
+const getRandomNum = () => {
+  return Math.floor(Math.random() * 25) + 1;
+};
+
+updateGrid.addEventListener("click", function () {
+  let mySet = new Set();
+  for (let i = 1; i <= 26; i++) {
+    console.log(`Run #${i}`);
+    let num = getRandomNum();
+    while (mySet.has[num]) {
+      num = getRandomNum();
+    }
+    console.log(`New num: ${num}`);
+    mySet.add(num);
+    console.log(mySet);
+    document.querySelector(`.letter-${i}`).textContent = abc[num];
+  }
+  console.log(mySet);
+});

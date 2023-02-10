c1e408d709fe711ae28128014945c3b7f87be7ef
Berny Peralta <bernardoperalta260@gmail.com>
2023-02-10 00:24:45 -0700

    caesar encryption is working now

--- script.js
+++ script.js
@@ -4,37 +4,88 @@
 const updateGrid = document.querySelector(".updateGrid");
 const textCaesar = document.querySelector(".textCaesar");
 const textSquare = document.querySelector(".textSquare");
+const scrollBar = document.getElementById("scrollNum");
+const scrollBarNum = document.querySelector(".scrollBarNum");
+const textInput = document.querySelector(".textInput");
 
 const abc = {
-  1: "a",
-  2: "b",
-  3: "c",
-  4: "d",
-  5: "e",
-  6: "f",
-  7: "g",
-  8: "h",
-  9: "i",
-  10: "j",
-  11: "k",
-  12: "l",
-  13: "m",
-  14: "n",
-  15: "o",
-  16: "p",
-  17: "q",
-  18: "r",
-  19: "s",
-  20: "t",
-  21: "u",
-  22: "v",
-  23: "w",
-  24: "x",
-  25: "y",
+  0: "a",
+  1: "b",
+  2: "c",
+  3: "d",
+  4: "e",
+  5: "f",
+  6: "g",
+  7: "h",
+  8: "i",
+  9: "j",
+  10: "k",
+  11: "l",
+  12: "m",
+  13: "n",
+  14: "o",
+  15: "p",
+  16: "q",
+  17: "r",
+  18: "s",
+  19: "t",
+  20: "u",
+  21: "v",
+  22: "w",
+  23: "x",
+  24: "y",
+  25: "z",
+};
+
+const abcKeyValueReverse = {
+  a: 0,
+  b: 1,
+  c: 2,
+  d: 3,
+  e: 4,
+  f: 5,
+  g: 6,
+  h: 7,
+  i: 8,
+  j: 9,
+  k: 10,
+  l: 11,
+  m: 12,
+  n: 13,
+  o: 14,
+  p: 15,
+  q: 16,
+  r: 17,
+  s: 18,
+  t: 19,
+  u: 20,
+  v: 21,
+  w: 22,
+  x: 23,
+  y: 24,
+  z: 25,
 };
 
 const getRandomNum = () => {
   return Math.floor(Math.random() * 25) + 1;
+};
+
+const newCharCaesar = (char, encryptionNum) => {
+  const newIndex = (abcKeyValueReverse[char] + parseInt(encryptionNum)) % 26;
+  return abc[newIndex];
+};
+
+const encryptionCaesar = (strInput, encryptionNum) => {
+  const arrStrInput = strInput.split("");
+  let encryptedStr = "";
+  arrStrInput.forEach((char) => {
+    if (/^[a-zA-Z]+$/.test(char)) {
+      encryptedStr += newCharCaesar(char, encryptionNum);
+    } else {
+      encryptedStr += char;
+    }
+  });
+  return encryptedStr.toUpperCase();
 };
 
 updateGrid.addEventListener("click", function () {
@@ -47,4 +98,14 @@ updateGrid.addEventListener("click", function () {
     mySet.add(num);
     document.querySelector(`.letter-${i}`).textContent = abc[num].toUpperCase();
   }
+});
+
+scrollBar.addEventListener("input", function () {
+  scrollBarNum.textContent = scrollBar.value;
+  textCaesar.textContent = encryptionCaesar(textInput.value, scrollBar.value);
+});
+
+textInput.addEventListener("input", function () {
+  textCaesar.textContent = encryptionCaesar(textInput.value, scrollBar.value);
+  textSquare.textContent = textInput.value;
 });
